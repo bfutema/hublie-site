393f6f12f63b76dfde56a3d6a5dd6a8d0f5bc978
Bruno Futema <bruno.futema@outlook.com>
2023-01-30 02:39:41 -0300

    fix: :hammer: remove navbar component, this component has already been removed

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 
-import { Navbar } from '@/components/Navbar';
-
 import { Container } from './styles';
 
 const Home: React.FC = () => {
   return (
     <Container>
-      <Navbar />
-
       <h1>home</h1>
     </Container>
   );
