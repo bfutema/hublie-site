8a21f5b7d71113d992165af29ee99e764e4490e5
Bruno Futema <bruno.futema@outlook.com>
2023-01-30 00:16:05 -0300

    refactor: :recycle: add ts config paths and adjust styled components to SSR

---
+++ src/interfaces/Theme.ts
@@ -0,0 +1,5 @@
+import { theme } from '@/styles/themes';
+
+export type ThemeType = {
+  theme: typeof theme;
+};
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,33 +1,96 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from 'next/document';
+import Script from 'next/script';
+import { ServerStyleSheet } from 'styled-components';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <link rel="stylesheet" href="https://app.boteria.com.br/cdn/webchat/webchat.v2.css" />
+type Props = DocumentContext;
+type Response = DocumentInitialProps;
 
-        <Script src="https://app.boteria.com.br/cdn/libs/showdown.min.js" strategy="beforeInteractive" />
-        <Script src="https://app.boteria.com.br/cdn/libs/axios.js" strategy="beforeInteractive" />
-        <Script src="https://app.boteria.com.br/cdn/libs/socket.io.js" strategy="beforeInteractive" />
-        <Script src="https://app.boteria.com.br/cdn/webchat/webchat.js" strategy="beforeInteractive" />
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: Props): Promise<Response> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-        <Script id="code7" strategy="afterInteractive">
-          {`window.renderBotWidget('634da1b79ea0f500181dc583');`}
-        </Script>
-      </Head>
+    try {
+      ctx.renderPage = () => {
+        return originalRenderPage({
+          enhanceApp: (App) => (props) => {
+            return sheet.collectStyles(<App {...props} />);
+          },
+        });
+      };
 
-      <body>
-        <Main />
+      const initialProps = await Document.getInitialProps(ctx);
 
-        <NextScript />
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
 
-        <Script src="https://plugin.handtalk.me/web/latest/handtalk.min.js" strategy="beforeInteractive" />
+  render(): JSX.Element {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://app.boteria.com.br/cdn/webchat/webchat.v2.css"
+          />
+        </Head>
 
-        <Script id="hand-talk" strategy="afterInteractive">
-          {`var ht = new HT({ token: "62af5efa5403fedaae8ea9f6eada1a24", avatar:"MAYA", pageSpeech: true });`}
-        </Script>
-      </body>
-    </Html>
-  )
+        <body>
+          <Main />
+
+          <NextScript />
+
+          <Script
+            src="https://app.boteria.com.br/cdn/libs/showdown.min.js"
+            strategy="beforeInteractive"
+          />
+
+          <Script
+            src="https://app.boteria.com.br/cdn/libs/axios.js"
+            strategy="beforeInteractive"
+          />
+
+          <Script
+            src="https://app.boteria.com.br/cdn/libs/socket.io.js"
+            strategy="beforeInteractive"
+          />
+
+          <Script
+            src="https://app.boteria.com.br/cdn/webchat/webchat.js"
+            strategy="beforeInteractive"
+          />
+
+          <Script id="code7" strategy="afterInteractive">
+            {`window.renderBotWidget('634da1b79ea0f500181dc583');`}
+          </Script>
+
+          <Script
+            src="https://plugin.handtalk.me/web/latest/handtalk.min.js"
+            strategy="beforeInteractive"
+          />
+
+          <Script id="hand-talk" strategy="afterInteractive">
+            {`var ht = new HT({ token: "62af5efa5403fedaae8ea9f6eada1a24", avatar:"MAYA", pageSpeech: true });`}
+          </Script>
+        </body>
+      </Html>
+    );
+  }
 }
